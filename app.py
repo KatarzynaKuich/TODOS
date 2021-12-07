9f8e3e579ef8a248750ab7147661935558368d78
Katarzyna Kuich <89656991+KatarzynaKuich@users.noreply.github.com>
2021-12-07 23:55:16 +0100

    LISTA ZADAN

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
-from flask import Flask, request, render_template, redirect, url_for
-from forms import TodoForm
+from flask import Flask, render_template,redirect,url_for,request
 from models import todos
-
+from forms import TodoForm
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "nininini"
 
-@app.route("/todos/", methods=["GET", "POST"])
+@app.route("/", methods=["GET", "POST"])
 def todos_list():
     form = TodoForm()
     error = ""
@@ -17,18 +16,6 @@ def todos_list():
         return redirect(url_for("todos_list"))
 
     return render_template("todos.html", form=form, todos=todos.all(), error=error)
-
-
-@app.route("/todos/<int:todo_id>/", methods=["GET", "POST"])
-def todo_details(todo_id):
-    todo = todos.get(todo_id - 1)
-    form = TodoForm(data=todo)
-
-    if request.method == "POST":
-        if form.validate_on_submit():
-            todos.update(todo_id - 1, form.data)
-        return redirect(url_for("todos_list"))
-    return render_template("todo.html", form=form, todo_id=todo_id)
 
 
 if __name__ == "__main__":
--- forms.py
+++ forms.py
@@ -1,14 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,IntegerField,FormField
-from wtforms.validators import DataRequired, Email
-
-
-#todos = [ {'title': 'Zakupy','description': 'Mleko, jajka, mąka, olej, papier toaletowy (jak będzie)',
-#        'done': False  },{'title': 'Zrobić zadania z Pythona','description': 'Przygotować projekt z modułu i wysłać Mentorowi',
- #       'done': False    }
+from wtforms import StringField,BooleanField,TextAreaField
 
 class TodoForm(FlaskForm):
+    id =StringField()
     title   = StringField()
-    description    = StringField()
-    done = FormField(FlaskForm)
+    description    = TextAreaField()
+    done = BooleanField()
 
